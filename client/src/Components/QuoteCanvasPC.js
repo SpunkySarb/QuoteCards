752cc62a6f98eda42c797b15e452b4725171d93b
Sarb <59626972+SpunkySarb@users.noreply.github.com>
2025-08-09 00:49:10 -0400

    Separate canvas dimension measurement from drawing

--- client/src/Components/QuoteCanvasPC.js
+++ client/src/Components/QuoteCanvasPC.js
@@ -2,9 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import canvasTxt from 'canvas-txt';
 
-
 const QuoteCanvasPC = () => {
-
     const color = useSelector(state => state.color);
     const fontColor = useSelector(state => state.fontColor);
     const quote = useRef();
@@ -15,66 +13,46 @@ const QuoteCanvasPC = () => {
     const [canvasDimentions, setDimentions] = useState({ width: 200, height: 300 });
 
     useEffect(() => {
-
-
         const divQuote = document.getElementById('myQuote');
+        if (!divQuote) return;
+        const width = divQuote.offsetWidth;
+        const height = divQuote.offsetHeight;
+        setDimentions(prev => (prev.width !== width || prev.height !== height ? { width, height } : prev));
+    }, []);
 
-        setDimentions({ width: divQuote.offsetWidth, height: divQuote.offsetHeight  });
-
-        console.log(divQuote.width, "  ", divQuote.height)
-
+    useEffect(() => {
         const canvas = quote.current;
-
+        if (!canvas) return;
         const ctx = canvas.getContext("2d");
 
         ctx.rect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = color;
         ctx.fill();
 
-        canvasTxt.fontSize = 32*5;
+        canvasTxt.fontSize = 32 * 5;
         ctx.fillStyle = fontColor;
         canvasTxt.font = fontStyle;
-        //canvasTxt.fontColor = "black";
+        canvasTxt.lineHeight = 50 * 5;
+        canvasTxt.drawText(ctx, text, 0, 0, canvas.width - 20, canvas.height - 350);
 
-
-
-
-
-
-
-
-        //  ctx.font = "100px " + fontStyle;
-        //  ctx.fillStyle = fontColor;
-        //  ctx.fillText("Sarb", canvas.width / 2.5, canvas.height / 2);
-        canvasTxt.lineHeight= 50*5;
-        canvasTxt.drawText(ctx, text , 0, 0, quote.current.width - 20, quote.current.height - 350)
         canvasTxt.fontSize = 20 * 5;
-        
-        canvasTxt.drawText(ctx, "-" + signature, 0, canvasDimentions.height*2, quote.current.width - 20, quote.current.height - 100)
+        canvasTxt.drawText(ctx, "-" + signature, 0, canvasDimentions.height * 2, canvas.width - 20, canvas.height - 100);
 
+    }, [color, fontStyle, fontColor, text, signature, canvasDimentions]);
 
-
-
-    }, [color, fontStyle, fontColor, canvasDimentions]);
-
-
-
-
-    return (<canvas className="w3-hide" style={{
-        width: canvasDimentions.width,
-        height: canvasDimentions.height
-    }} width={`${canvasDimentions.width * 5}`} height={`${canvasDimentions.height * 5}`} id="quoteCanvasPC" ref={quote}>
-
-
-
-
-
-
-    </canvas>);
-
-
-
-}
-
+    return (
+        <canvas
+            className="w3-hide"
+            style={{
+                width: canvasDimentions.width,
+                height: canvasDimentions.height
+            }}
+            width={canvasDimentions.width * 5}
+            height={canvasDimentions.height * 5}
+            id="quoteCanvasPC"
+            ref={quote}
+        ></canvas>
+    );
+};
 
 export default QuoteCanvasPC;
