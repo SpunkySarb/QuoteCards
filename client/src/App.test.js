19cd9e5e2e45ff6146afb5e0f7629effd22d13ef
Sarb <59626972+SpunkySarb@users.noreply.github.com>
2025-08-09 02:01:47 -0400

    Remove unused selector from phone quote holder

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './utils/Store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders header', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const headerElement = screen.getByText(/Quote Cards/i);
+  expect(headerElement).toBeInTheDocument();
 });
